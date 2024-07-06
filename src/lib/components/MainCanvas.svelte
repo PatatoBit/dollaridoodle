0223a019f2c431a21fe18813630fcd5a8cb965a5
PatatoBit <pathonscript@gmail.com>
2024-07-06 15:34:37 +0700

    updated todo

---
+++ src/lib/components/MainCanvas.svelte
@@ -0,0 +1,20 @@
+<div class="container">
+	<div></div>
+	<canvas></canvas>
+</div>
+
+<style lang="scss">
+	.container {
+		position: relative;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+
+	canvas {
+		aspect-ratio: 3/2;
+		width: 40rem;
+		height: auto;
+		border: 1px dashed black;
+	}
+</style>
