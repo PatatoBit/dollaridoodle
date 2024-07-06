5b38c914a2b4cd12ff80a4c73806d3281ac6f3a3
PatatoBit <pathonscript@gmail.com>
2024-07-06 15:55:39 +0700

    zooming in and out

--- src/lib/components/MainCanvas.svelte
+++ src/lib/components/MainCanvas.svelte
@@ -1,6 +1,77 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	let canvas: HTMLCanvasElement;
+	let ctx: CanvasRenderingContext2D;
+	let scale = 1;
+	let originX = 0;
+	let originY = 0;
+	let isDragging = false;
+	let startX: number;
+	let startY: number;
+
+	onMount(() => {
+		ctx = canvas.getContext('2d')!;
+		canvas.width = 2000; // Set canvas dimensions
+		canvas.height = 2000;
+
+		// Draw an example grid or initial doodles
+		drawTest();
+	});
+
+	function drawTest() {
+		ctx.beginPath();
+
+		// Bigger circle
+		ctx.arc(1000, 1000, 60, 0, 2 * Math.PI);
+
+		ctx.stroke();
+	}
+
+	function handleWheel(event: WheelEvent) {
+		event.preventDefault();
+
+		if (event.deltaY > 0) {
+			if (scale <= 0.5) return;
+			scale -= 0.1;
+		} else {
+			if (scale >= 10) return;
+			scale += 0.1;
+		}
+
+		canvas.animate(
+			[
+				{
+					transform: `scale(${scale})`
+				}
+			],
+			{
+				duration: 100,
+				easing: 'ease-in-out',
+				fill: 'forwards'
+			}
+		);
+	}
+
+	function testScale() {
+		canvas.animate(
+			[
+				{
+					transform: `scale(${scale * 2})`
+				}
+			],
+			{
+				duration: 100,
+				easing: 'ease-in-out',
+				fill: 'forwards'
+			}
+		);
+	}
+</script>
+
 <div class="container">
-	<div></div>
-	<canvas></canvas>
+	<div class="canvas-container" on:wheel={handleWheel} on:click={testScale}>
+		<canvas bind:this={canvas}></canvas>
+	</div>
 </div>
 
 <style lang="scss">
@@ -11,10 +82,20 @@
 		align-items: center;
 	}
 
+	.canvas-container {
+		overflow: hidden;
+
+		border: 1px dashed black;
+	}
+
 	canvas {
 		aspect-ratio: 3/2;
 		width: 40rem;
 		height: auto;
-		border: 1px dashed black;
+
+		cursor: grab;
+		&:active {
+			cursor: grabbing;
+		}
 	}
 </style>
