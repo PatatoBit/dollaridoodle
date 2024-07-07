ef3e11e50273c9a8fc64feb651b9d782149af262
Patato <pathonscript@gmail.com>
2024-07-07 19:02:43 +0700

    added default cube to initialize

--- src/lib/components/MainCanvas.svelte
+++ src/lib/components/MainCanvas.svelte
@@ -1,61 +1,55 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import { Canvas, Rect } from 'fabric';
+	import { Canvas, PencilBrush, Rect } from 'fabric';
 
 	let canvas: Canvas | null = null;
 
 	onMount(() => {
 		// Initialize Fabric.js canvas
+
 		canvas = new Canvas('c', {
 			isDrawingMode: true,
-			width: 500,
-			height: 500,
+			width: 600,
+			height: 400,
 			backgroundColor: '#f0f0f0'
 		});
 
-		// Set up the drawing brush
-		if (canvas.freeDrawingBrush) {
-			let brush = canvas.freeDrawingBrush;
-			brush.color = 'black';
-			brush.width = 10;
-		}
-
-		// Create and add a rectangle object to test the canvas
-		const rect = new Rect({
+		let rect = new Rect({
 			left: 100,
 			top: 100,
 			fill: 'red',
 			width: 20,
 			height: 20
 		});
-		canvas?.add(rect);
+
+		canvas.add();
+		canvas.remove();
+
+		let brush = new PencilBrush(canvas);
+		brush.color = 'red';
+		brush.width = 4;
+		canvas.freeDrawingBrush = brush;
+
+		console.log(canvas);
 	});
 
-	function clearCanvas() {
-		if (canvas) {
-			canvas.clear();
-		}
+	function toggleDrawingMode() {
+		canvas!.isDrawingMode = !canvas!.isDrawingMode;
 	}
 </script>
 
 <div class="canvas-container">
 	<canvas id="c"></canvas>
-	<button on:click={clearCanvas}>Clear Canvas</button>
 </div>
+<button on:click={toggleDrawingMode}>{canvas?.isDrawingMode}</button>
 
 <style lang="scss">
 	.canvas-container {
-		overflow: hidden;
 		border: 1px dashed black;
 		aspect-ratio: 3/2;
 	}
 
 	canvas {
 		height: auto;
-
-		cursor: grab;
-		&:active {
-			cursor: grabbing;
-		}
 	}
 </style>
