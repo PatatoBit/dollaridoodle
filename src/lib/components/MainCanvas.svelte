a23df683afc4bfdd1a78ef1c3993b886c6f783ba
Patato <pathonscript@gmail.com>
2024-07-07 15:37:04 +0700

    setup canvas drawing mode

--- src/lib/components/MainCanvas.svelte
+++ src/lib/components/MainCanvas.svelte
@@ -1,19 +1,46 @@
 <script lang="ts">
-	import { Canvas } from 'fabric';
 	import { onMount } from 'svelte';
+	import { Canvas, Rect } from 'fabric';
 
 	let canvas: Canvas | null = null;
+
 	onMount(() => {
+		// Initialize Fabric.js canvas
 		canvas = new Canvas('c', {
-			width: 1000,
-			height: 1000,
+			isDrawingMode: true,
+			width: 500,
+			height: 500,
 			backgroundColor: '#f0f0f0'
 		});
+
+		// Set up the drawing brush
+		if (canvas.freeDrawingBrush) {
+			let brush = canvas.freeDrawingBrush;
+			brush.color = 'black';
+			brush.width = 10;
+		}
+
+		// Create and add a rectangle object to test the canvas
+		const rect = new Rect({
+			left: 100,
+			top: 100,
+			fill: 'red',
+			width: 20,
+			height: 20
+		});
+		canvas?.add(rect);
 	});
+
+	function clearCanvas() {
+		if (canvas) {
+			canvas.clear();
+		}
+	}
 </script>
 
 <div class="canvas-container">
 	<canvas id="c"></canvas>
+	<button on:click={clearCanvas}>Clear Canvas</button>
 </div>
 
 <style lang="scss">
@@ -21,7 +48,6 @@
 		overflow: hidden;
 		border: 1px dashed black;
 		aspect-ratio: 3/2;
-		width: 40rem;
 	}
 
 	canvas {
