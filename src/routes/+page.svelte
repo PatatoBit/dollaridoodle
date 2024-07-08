d160e0f1e78d6d53f2a5b3c0de780946b42a3fc0
Patato <pathonscript@gmail.com>
2024-07-08 20:52:18 +0700

    homepage layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,18 +3,28 @@
 </script>
 
 <main>
-	<h1>DollariDoodle</h1>
+	<div class="main-container">
+		<h1 class="main-title">DollariDoodle</h1>
 
-	<div class="canvas-container">
-		<MainCanvas />
+		<div class="canvas-container">
+			<MainCanvas />
+		</div>
+
+		<div class="details">
+			<p>412 requests have been completed</p>
+			<p>1,012 requests in queue...</p>
+		</div>
+
+		<div class="input-bar">
+			<input type="text" placeholder="Search or request a doodle" />
+			<button>Send</button>
+		</div>
 	</div>
 </main>
 
 <style lang="scss">
 	.canvas-container {
 		position: relative;
-
-		border: 1px solid red;
 	}
 
 	main {
@@ -22,6 +32,25 @@
 		flex-direction: column;
 		align-items: center;
 
+		width: 100%;
 		height: 100vh;
+	}
+
+	.main-container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		height: 100%;
+		gap: 2rem;
+	}
+
+	.main-title {
+		margin-top: 4rem;
+	}
+
+	.input-bar {
+		width: 100%;
+		display: flex;
+		gap: 1rem;
 	}
 </style>
