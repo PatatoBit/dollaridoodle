563e621e5401438f83f70ac5c4ac9369ff4e2699
Patato <pathonscript@gmail.com>
2024-07-18 15:13:02 +0700

    centering

--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -1,8 +1,29 @@
 <img class="logo" src="/images/logo.png" alt="DollariDoodle Logo" />
-<main></main>
+
+<main>
+	<div class="wrapper">
+		<div class="canvas"></div>
+
+		<div class="input-bar">
+			<input type="text" />
+			<button></button>
+		</div>
+	</div>
+</main>
 
 <style lang="scss">
 	.logo {
 		width: 25rem;
+	}
+
+	.input-bar {
+		display: flex;
+		flex-direction: row;
+	}
+
+	.canvas {
+		width: 100%;
+		aspect-ratio: 5/3;
+		border: 2px solid black;
 	}
 </style>
