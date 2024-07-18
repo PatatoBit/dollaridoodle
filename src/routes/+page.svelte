906deb61141b47d23830f524e054d33b73db015d
Patato <pathonscript@gmail.com>
2024-07-18 20:56:54 +0700

    landing page content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,56 @@
-<main>Landing page</main>
+<main class="center">
+	<div class="wrapper">
+		<h1 class="fancy">Generate your art ideas</h1>
+		<h1 class="fancy">but with a <span class="mono">human</span></h1>
 
-<a href="/app">show me</a>
+		<p>image generation app but a human draws instead of AI</p>
+
+		<a href="/app">show me</a>
+	</div>
+</main>
+
+<article class="center">
+	<div class="wrapper">
+		<div class="abouts">
+			<div class="about">
+				<div class="about-image"></div>
+
+				<h4>Request a doodle</h4>
+			</div>
+
+			<div class="about">
+				<div class="about-image"></div>
+
+				<h4>A human draws your idea</h4>
+				<p>instead of an AI model</p>
+			</div>
+
+			<div class="about">
+				<div class="about-image"></div>
+
+				<h4>Use it in any way you want</h4>
+				<p>it's completely yours</p>
+			</div>
+		</div>
+	</div>
+</article>
+
+<style lang="scss">
+	.center {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+
+		width: 100%;
+		min-height: 65vh;
+	}
+
+	.abouts {
+		width: 100%;
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+		gap: 5rem;
+	}
+</style>
