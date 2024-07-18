49b241d5c6c7238dab5e32dcb09f787f77d7519d
Patato <pathonscript@gmail.com>
2024-07-18 15:01:17 +0700

    page auth flow

--- src/lib/components/MainCanvas.svelte
+++ src/lib/components/MainCanvas.svelte
@@ -1,45 +1,8 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import { Canvas, PencilBrush, Rect } from 'fabric';
-
-	let canvas: Canvas | null = null;
-
-	onMount(() => {
-		// Initialize Fabric.js canvas
-
-		canvas = new Canvas('c', {
-			isDrawingMode: true,
-			width: 600,
-			height: 400,
-			backgroundColor: '#f0f0f0'
-		});
-
-		let rect = new Rect({
-			left: 100,
-			top: 100,
-			fill: 'red',
-			width: 20,
-			height: 20
-		});
-
-		canvas.add(rect);
-		canvas.remove(rect);
-
-		let brush = new PencilBrush(canvas);
-		brush.color = '#212121';
-		brush.width = 4;
-		canvas.freeDrawingBrush = brush;
-
-		console.log(canvas);
-	});
-
-	function toggleDrawingMode() {
-		canvas!.isDrawingMode = !canvas!.isDrawingMode;
-	}
 </script>
 
 <div class="container">
-	<canvas id="c"></canvas>
+	<canvas></canvas>
 </div>
 
 <style lang="scss">
---
+++ src/routes/app/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import Login from '$lib/components/Login.svelte';
+</script>
+
+{#if true}
+	<slot />
+{:else}
+	<Login />
+{/if}
