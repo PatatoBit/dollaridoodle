96bedf3fdc35e811990610cbc9fe39e5ef0f3447
PatatoBit <pathonscript@gmail.com>
2024-07-19 22:14:06 +0700

    basic styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,36 +1,42 @@
-<main class="center">
-	<div class="wrapper">
+<main class="page">
+	<div class="wrapper landing">
 		<h1 class="fancy hero">Generate your art ideas</h1>
 		<h1 class="fancy hero">but with a <span class="mono">human</span></h1>
 
 		<p>image generation but a human draws instead of AI</p>
 
-		<a href="/app">show me</a>
+		<a href="/app"> <button>Show me</button></a>
 	</div>
 </main>
 
-<article class="center">
+<article class="page">
 	<div class="wrapper">
 		<div class="abouts">
 			<div class="about">
 				<div class="about-image"></div>
 
-				<h4>Request a doodle</h4>
-				<p>anything your mind can think of</p>
+				<div class="about-text">
+					<h4>Request a doodle</h4>
+					<p>anything you can think of</p>
+				</div>
 			</div>
 
 			<div class="about">
 				<div class="about-image"></div>
 
-				<h4>A human draws your idea</h4>
-				<p>instead of an AI model</p>
+				<div class="about-text">
+					<h4>A human draws your idea</h4>
+					<p>instead of an AI model</p>
+				</div>
 			</div>
 
 			<div class="about">
 				<div class="about-image"></div>
 
-				<h4>Use it in any way you want</h4>
-				<p>it's completely yours</p>
+				<div class="about-text">
+					<h4>Use it in any way you want</h4>
+					<p>it's completely yours</p>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -39,6 +45,10 @@
 <style lang="scss">
 	.hero {
 		color: var(--primary);
+	}
+
+	.landing {
+		margin-top: 7rem;
 	}
 
 	.center {
@@ -63,6 +73,10 @@
 			flex-direction: column;
 			align-items: center;
 			gap: 1rem;
+
+			.about-text {
+				text-align: center;
+			}
 
 			.about-image {
 				width: 100px;
