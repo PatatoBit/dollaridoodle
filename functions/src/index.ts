d5454ea0b17a02c2c9c744b4f415694413f27dbb
PatatoBit <pathonscript@gmail.com>
2024-07-21 22:17:13 +0700

    refactoring v2 functions env var

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,7 +10,6 @@
 import { onRequest } from 'firebase-functions/v2/https';
 import * as logger from 'firebase-functions/logger';
 import * as admin from 'firebase-admin';
-import * as functions from 'firebase-functions';
 import Stripe from 'stripe';
 import { defineString } from 'firebase-functions/params';
 
@@ -24,37 +23,40 @@ export const helloWorld = onRequest((request, response) => {
 	response.send('Hello from Firebase!');
 });
 
-export const handleStripeWebhook = onRequest(async (req, res) => {
-	const stripeSecretKey = defineString('STRIPE_SECRET_KEY');
-	const stripeWebhookSecret = defineString('STRIPE_PAYMENT_WEBHOOK_SECRET');
+const stripeSecretKey = defineString('STRIPE_SECRET_KEY');
+const stripeWebhookSecret = defineString('STRIPE_PAYMENT_WEBHOOK_SECRET');
 
-	const stripe = new Stripe(stripeSecretKey.value());
-	const sig = req.headers['stripe-signature'] as string;
+export const handleStripeWebhook = onRequest(
+	{ secrets: [stripeSecretKey, stripeWebhookSecret] },
+	async (req, res) => {
+		const stripe = new Stripe(stripeSecretKey.value());
+		const sig = req.headers['stripe-signature'] as string;
 
-	let event: Stripe.Event;
+		let event: Stripe.Event;
 
-	try {
-		event = stripe.webhooks.constructEvent(req.rawBody, sig, stripeWebhookSecret.value());
-	} catch (err) {
-		console.error('⚠️ Webhook signature verification failed.', (err as Error).message);
-		res.status(400).send(`Webhook Error: ${(err as Error).message}`);
-		return;
-	}
-
-	if (event.type === 'checkout.session.completed') {
-		const session = event.data.object as Stripe.Checkout.Session;
-		const payloadData = JSON.parse(session.metadata?.payloadData as string);
-
-		if (payloadData) {
-			try {
-				await admin.firestore().collection('doodles').add(payloadData);
-			} catch (error) {
-				console.error('Error updating document:', error);
-			}
-		} else {
-			console.error('No payload data recieved');
+		try {
+			event = stripe.webhooks.constructEvent(req.rawBody, sig, stripeWebhookSecret.value());
+		} catch (err) {
+			console.error('⚠️ Webhook signature verification failed.', (err as Error).message);
+			res.status(400).send(`Webhook Error: ${(err as Error).message}`);
+			return;
 		}
-	}
 
-	res.status(200).json({ received: true });
-});
+		if (event.type === 'checkout.session.completed') {
+			const session = event.data.object as Stripe.Checkout.Session;
+			const payloadData = JSON.parse(session.metadata?.payloadData as string);
+
+			if (payloadData) {
+				try {
+					await admin.firestore().collection('doodles').add(payloadData);
+				} catch (error) {
+					console.error('Error updating document:', error);
+				}
+			} else {
+				console.error('No payload data recieved');
+			}
+		}
+
+		res.status(200).json({ received: true });
+	}
+);
