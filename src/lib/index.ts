ad1b782fb7c1749522889dac505719e67bbe0cdc
Patato <pathonscript@gmail.com>
2024-07-24 21:04:17 +0700

    reading realtime data

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,6 @@
 // place files you want to import through the `$lib` alias in this folder.
+export interface RequestData {
+	prompt: string;
+	isPrivate: boolean;
+	isExpress: boolean;
+}
--- src/routes/app/request/+page.svelte
+++ src/routes/app/request/+page.svelte
@@ -1,11 +1,34 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import type { RequestData } from '$lib';
+	import { db } from '$lib/firebase';
+	import { doc, onSnapshot } from 'firebase/firestore';
 
 	const idString = $page.url.searchParams.get('id');
+	const docRef = idString ? doc(db, 'requests', idString) : null;
+
+	let data: RequestData;
+
+	if (docRef) {
+		onSnapshot(docRef, (doc) => {
+			if (doc.exists()) {
+				data = doc.data() as RequestData;
+			} else {
+				console.error('No such document!');
+			}
+		});
+	}
 </script>
 
 <div class="page">
 	<div class="wrapper">
-		<p>{idString}</p>
+		{#if idString}
+			<!-- content here -->
+			<p>id: {idString}</p>
+			<p>data: {JSON.stringify(data)}</p>
+		{:else}
+			no id
+			<!-- else content here -->
+		{/if}
 	</div>
 </div>
