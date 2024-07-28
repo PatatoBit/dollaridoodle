d61d1a927c7241909a586c8b12ed7ba14a551239
PatatoBit <pathonscript@gmail.com>
2024-07-28 21:03:47 +0700

    admin check

--- src/routes/admin/+layout.svelte
+++ src/routes/admin/+layout.svelte
@@ -1,10 +1,19 @@
 <script lang="ts">
 	import { SignedIn, SignedOut } from 'sveltefire';
-	import { SignInWithGoogle } from '$lib/utils';
+	import { SignInWithGoogle, SignOut } from '$lib/utils';
 </script>
 
-<SignedIn>
-	<slot></slot>
+<SignedIn let:auth>
+	{#if auth.currentUser?.email == 'pathonscript@gmail.com'}
+		<slot />
+	{:else}
+		<div class="center">
+			<h2>You're not an admin</h2>
+			<p>{auth.currentUser?.displayName}</p>
+			<br />
+			<button on:click={() => SignOut(auth)}>Log out</button>
+		</div>
+	{/if}
 </SignedIn>
 
 <SignedOut let:auth>
